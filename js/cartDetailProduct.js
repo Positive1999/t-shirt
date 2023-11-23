25e073c831a4c129fa47a206d5590651efed692a
TVHieu99 <hieutrantran102@gmail.com>
2023-11-23 11:07:02 +0700

    add

---
+++ js/cartDetailProduct.js
@@ -0,0 +1,40 @@
+var container = document.getElementById("cart-item-list");
+
+function renderCartItems() {
+  container.innerHTML = ""; // Xóa các sản phẩm hiện tại trên giao diện trước khi render lại
+
+  var productsData = sessionStorage.getItem("cartItems");
+  var products = JSON.parse(productsData);
+
+  products.forEach(function(product, index) {
+    var productDiv = document.createElement("div");
+
+    var productName = document.createElement("span");
+    var productImage = document.createElement("img");
+    var productPrice = document.createElement("p");
+    var productRemove = document.createElement("button");
+
+    productName.textContent = product.name;
+    productImage.src = product.image;
+    productPrice.textContent = product.price;
+    productRemove.textContent = 'Delete product';
+
+    // Thêm sự kiện nghe cho nút "Delete product"
+    productRemove.addEventListener('click', function() {
+      // Xóa sản phẩm khỏi giao diện
+      container.removeChild(productDiv);
+      // Xóa sản phẩm khỏi sessionStorage
+      products.splice(index, 1);
+      sessionStorage.setItem("cartItems", JSON.stringify(products));
+    });
+
+    productDiv.appendChild(productName);
+    productDiv.appendChild(productImage);
+    productDiv.appendChild(productPrice);
+    productDiv.appendChild(productRemove);
+
+    container.appendChild(productDiv);
+  });
+}
+
+renderCartItems();
