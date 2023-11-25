ac45953bd10a4d01f5d5594a0aa0438e8a40efb4
TVHieu99 <hieutrantran102@gmail.com>
2023-11-25 12:00:39 +0700

    add

--- js/index2.js
+++ js/index2.js
@@ -9,6 +9,7 @@ fetch('data/home.json')
       listProducts.classList.add('product', 'col-xs-12', 'col-sm-6', 'col-md-4', 'col-lg-3');
 
       listProducts.innerHTML = `
+      <div class="sale">-10%</div>
       <img src="${e.image}">
         <h3>${e.name}</h3>
         <p>${e.price}</p>
@@ -19,7 +20,6 @@ fetch('data/home.json')
           </button>
         </div>
       `;
-
       listPro.appendChild(listProducts);
     });
 
@@ -59,64 +59,3 @@ fetch('data/home.json')
       cartCounter.textContent = cartItems.length;
     }
   });
-
-function showCart() {
-  const cartItemList = document.querySelector('.cart-item-list');
-  const cartCounter = document.querySelector('.cart-counter');
-
-  cartItemList.innerHTML = '';
-
-  const emptyCartMessage = document.createElement('p');
-  emptyCartMessage.textContent = cartCounter.textContent === '0' ? 'Giỏ hàng rỗng!' : '';
-  emptyCartMessage.style.color = 'black';
-  cartItemList.appendChild(emptyCartMessage);
-
-  cartItemList.style.display = 'block';
-
-  // Hiển thị sản phẩm từ sessionStorage
-  const storedCartItems = JSON.parse(sessionStorage.getItem('cartItems')) || [];
-  storedCartItems.forEach(item => {
-    const Item = document.createElement('div');
-    Item.classList.add('ItemProductDetail');
-
-    const listItem = document.createElement('div');
-    const listItemImg = document.createElement('img');
-    const listItemPrice = document.createElement('div');
-    listItemPrice.classList.add('detailPrice');
-    listItem.textContent = item.name;
-    listItemImg.src = item.image;
-    listItemPrice.textContent = item.price;
-
-    Item.appendChild(listItem);
-    Item.appendChild(listItemImg);
-    Item.appendChild(listItemPrice);
-
-    cartItemList.appendChild(Item);
-  });
-}
-
-function hideCart() {
-  document.querySelector('.cart-item').style.display = 'none';
-  const overlay = document.querySelector('.overlay');
-  if (overlay) {
-    overlay.parentNode.removeChild(overlay);
-  }
-}
-
-const cartIcon = document.querySelector('.cart i');
-cartIcon.addEventListener('click', function () {
-  const cartItem = document.querySelector('.cart-item');
-  const body = document.body;
-
-  const overlay = document.createElement('div');
-  overlay.classList.add('overlay');
-  body.appendChild(overlay);
-
-  overlay.style.cssText = 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; background-color: rgba(0, 0, 0, 0.5); z-index: 999;';
-
-  cartItem.style.display = 'block';
-
-  overlay.addEventListener('click', function () {
-    hideCart();
-  });
-});
